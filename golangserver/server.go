544af2a3c2fa52a650e27ef357ab7b430796d64f
shigoswa <shigoswa@cisco.com>
2020-08-06 12:03:03 +0530

    modified server code

---
+++ golangserver/server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/rs/cors"
+)
+
+type Response struct {
+	TimeStamp time.Time `json:"timestamp"`
+	Greetings string    `json:"greeting"`
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(Response{time.Now(), "Hi there!"})
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir("testApp/dist/testApp")))
+	mux.HandleFunc("/endpoint", handler)
+	handler := cors.Default().Handler(mux)
+	http.ListenAndServe(":1234", handler)
+	// http.HandleFunc("/endpoint", handler)
+	// log.Fatal(http.ListenAndServe(":1234", nil))
+}
